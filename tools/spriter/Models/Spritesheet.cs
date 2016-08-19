52475ecb4e562db6ce79a167414bfb25265fd5ab
Josh Olson <Olson.dev@Gmail.com>
2016-08-19 17:38:18 -0500

    Programmatically generate spritesheets instead of using .tps files
    This allows dynamic filenames and further processing

---
+++ tools/spriter/Models/Spritesheet.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Spriter.Models {
+	public class Spritesheet {
+		public string Name { get; private set; }
+		public string Pattern { get; private set; }
+		public Func<string, string> Renamer { get; private set; }
+		public int MaxWidth { get; set; }
+		public int MaxHeight { get; set; }
+		public bool ForceSquare { get; set; }
+		public Spritesheet (string name, string pattern, Func<string, string> renamer = null) {
+			Name = name;
+			Pattern = pattern;
+			Renamer = renamer ?? ((filename) => filename);
+		}
+
+		public string BuildTexturePackerArguments() {
+			var args = "";
+			args += $"--sheet __{Name}.png ";
+			args += $"--data {Name}.xml --format xml ";
+			args += $"--trim-sprite-names --disable-rotation ";
+			if (MaxWidth > 0)  args += $"--max-width {MaxWidth} ";
+			if (MaxHeight > 0) args += $"--max-height {MaxHeight} ";
+			if (ForceSquare)   args += "--force-squared ";
+			args += $"--extrude 0 --shape-padding 2 --trim-mode Trim ";
+			args += Name;
+			return args;
+		}
+  }
+}
--- tools/spriter/Program.cs
+++ tools/spriter/Program.cs
@@ -1,21 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using ImageProcessorCore;
+using Spriter.Extensions;
 using Spriter.Models;
 using Spriter.Services;
 
-namespace Spriter {
-	public class Program {
+namespace Spriter
+{
+    public class Program {
 		static private void Main(string[] args) {
 			var app = new Program();
 			Console.WriteLine("Running...");
-			app.RunExtraction();
+			//app.RunExtraction();
 			//app.RunAtfInfo(args);
 			//app.RunRgbaToPng();
+			app.RunBuildSpritesheets();
 		}
 
 		private string gfxDirectory;
@@ -32,10 +38,88 @@ namespace Spriter {
 			file.SaveAsPng(pngPath);
 		}
 
+		private void RunBuildSpritesheets() {
+			var assemblyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+			var extracted = Path.Combine(assemblyPath, "extracted");
+			var baseSpritesheetDirectory = Path.Combine(assemblyPath, "spritesheets");
+			var sheets = new List<Spritesheet> {
+				new Spritesheet("banners"     , @"^Banners\\K[0-9]{2}\.png$", filename => filename.ToLower()),
+				new Spritesheet("classes"     , @"^Classes\.(?!blade|comingsoon)[a-z]+\.png$", filename => filename.Replace("Classes.", "")),
+				new Spritesheet("class-perks" , @"^ClassMenu2\.aug_.+\.png$", filename => filename.Replace("ClassMenu2.aug_", "")),
+				new Spritesheet("flags"       , @"^Options\.(de_DE|en_US|es_ES|fr_FR|it_IT|jp_JP|kr_KR|zh_CH|)\.png$", filename => filename.Replace("Options.", "").Replace('_', '-')),
+				new Spritesheet("kingdoms"    , @"^(MapLocations\.K[0-9]{2}|TroopCollection\.tab_(k32|k33|k34|hero))\.png$", filename => filename.Replace("MapLocations.", "").Replace("TroopCollection.tab_", "").ToLower()),
+				new Spritesheet("mana-colors" , @"^TroopCardAll\.(All|(Red|Orange|Yellow|Blue|Green|Purple|Brown){1,3})\.png$", filename => filename.Replace("TroopCardAll.", "").Replace("Orange", "Brown").PascalToSlug()) { MaxWidth = 1024, MaxHeight = 1024 },
+				new Spritesheet("misc"        , @"^((CraftingMenu\.(can_ascend|can_upgrade|can_upgrade_ascend))|(KingdomMenu\.(Attack|Heart|Magic|glory|gold|goldperday|goldperkingdom|rarity_stars|star[01]|star_off))|(MapLocations\.(kstars[0-5]|levelshield_[0-3]|nameplate.*))|(PuzzleIntro\.ButtonArrow.*)|(TroopCardAll\.(Armor|Armor_zero|TurnIndicator|card_level|disenchant_soul|stars_[0-5]|trait_flag_small_[1-3]))|(TroopCardTraits\.(trait_flag_[1-3]))|(TroopCollection\.(icon_kingdoms)))\.png$", filename => {
+					filename = Regex.Replace(filename, @"^(CraftingMenu|KingdomMenu|MapLocations|PuzzleIntro|TroopCardAll|TroopCardTraits|TroopCollection)\.", "");
+					switch (filename) {
+						case "ButtonArrow.png":     return "btn-left.png";
+						case "ButtonArrow2.png":    return "btn-right.png";
+						case "cardlevel.png":       return "troop-level.png";
+						case "disenchant_soul.png": return "soul.png";
+						case "goldperday.png":      return "gold-chest.png";
+						case "goldperkingdom.png":  return "gold-coin-stacks.png";
+						case "icon_kingdoms.png":   return "crown.png";
+						case "TurnIndicator.png":   return "turn-indicator.png";
+					}
+					filename = Regex.Replace(filename, @"^star([01])", @"star-$1");
+					filename = Regex.Replace(filename, @"^kstars", "stars-blk-");
+					filename = Regex.Replace(filename, @"^levelshield", "level-shield");
+					filename = Regex.Replace(filename, @"^trait_flag_([1-3])", "trait-v-$1");
+					filename = Regex.Replace(filename, @"^trait_flag_small", "trait-h");
+					return filename.Replace('_', '-').ToLower();
+				}),
+				new Spritesheet("traits"     , @"^Traits\.[a-z]+\.png$", filename => filename.Replace("Traits.", "")),
+				new Spritesheet("traitstones", @"^LargeRunes.Rune[0-9]{2}\.png$", filename => filename.Replace("LargeRunes.Rune", "rune")),
+				new Spritesheet("troop-card" , @"^TroopCard(Large|Spell).rare[0-9]\.png$", filename => filename.Replace("TroopCardLarge.rare", "frame-rarity-").Replace("TroopCardSpell.rare", "spell-rarity-")) { MaxWidth = 4096, MaxHeight = 4096 }
+			};
+
+
+			foreach (var sheet in sheets) {
+				var files = Directory.GetFiles(extracted, "*.png", SearchOption.AllDirectories);
+				var filtered = files.Where(path => Regex.IsMatch(path.Replace($@"{extracted}\", ""), sheet.Pattern)).ToList();
+				var spritesheetDirectory = Path.Combine(baseSpritesheetDirectory, sheet.Name);
+				Directory.CreateDirectory(spritesheetDirectory);
+				foreach (var file in filtered) {
+					var renamed = sheet.Renamer(Path.GetFileName(file));
+					File.Copy(file, Path.Combine(spritesheetDirectory, renamed), true);
+				}
+
+				var cmd = new ProcessStartInfo();
+				cmd.FileName = @"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe";
+				cmd.Arguments = sheet.BuildTexturePackerArguments();
+				cmd.WorkingDirectory = baseSpritesheetDirectory;
+				var process = Process.Start(cmd);
+				process.WaitForExit();
+
+				var atlas = XElement.Load(Path.Combine(baseSpritesheetDirectory, $"{sheet.Name}.xml"));
+				var width = int.Parse(atlas.Attribute("width").Value);
+				var height = int.Parse(atlas.Attribute("height").Value);
+				var img = new Image(width, height);
+				var pixels = img.Pixels;
+				foreach (var sprite in atlas.Elements("sprite")) {
+					var spriteFile = Path.Combine(spritesheetDirectory, $"{sprite.Attribute("n").Value}.png");
+					var spriteImage = new Image(File.OpenRead(spriteFile));
+					var spritePixels = spriteImage.Pixels;
+					var x = int.Parse(sprite.Attribute("x").Value);
+					var y = int.Parse(sprite.Attribute("y").Value);
+					var w = int.Parse(sprite.Attribute("w").Value);
+					var h = int.Parse(sprite.Attribute("h").Value);
+					var oX = int.Parse(sprite.Attribute("oX")?.Value ?? "0");
+					var oY = int.Parse(sprite.Attribute("oY")?.Value ?? "0");
+					var oW = int.Parse(sprite.Attribute("oW")?.Value ?? sprite.Attribute("w").Value);
+					for (var i = 0; i < h; i++) {
+						for (var j = 0; j < w; j++) {
+							pixels[(y + i) * width + x + j] = spritePixels[(i + oY) * oW + j + oX];
+						}
+					}
+				}
+				img.Save(File.OpenWrite(Path.Combine(baseSpritesheetDirectory, $"{sheet.Name}.png")));
+			}
+		}
+
 		private void RunExtraction() {
 			var locator = new GameLocator();
 			gfxDirectory = Path.GetFullPath(Path.Combine(locator.GameFolder(), "res/Graphics/3"));
-
 			Console.WriteLine("Processing atlas files...");
 			ProcessAtlasFiles();
 		}
@@ -61,7 +145,6 @@ namespace Spriter {
 			var assemblyPath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
 			var atlasFiles = Directory.GetFiles(gfxDirectory, "*.xml", SearchOption.AllDirectories);
 			Parallel.ForEach(atlasFiles, atlasFile => {
-
 				var atlas = XElement.Load(atlasFile);
 				if (atlas.Name.LocalName != "TextureAtlas") return;
 				var imagePath = atlas.Attribute("imagePath").Value;
