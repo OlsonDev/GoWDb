5f50a543412c18d85859d5bf9c47e13434b10527
Josh Olson <Olson.dev@Gmail.com>
2016-08-21 12:36:57 -0500

    Switch from ng2 to Polymer for new job reasons

--- tools/publicize-node-modules.js
+++ tools/publicize-node-modules.js
@@ -1,13 +1,17 @@
 let fs = require('fs-extra');
-let copy = (path) => fs.copySync(`node_modules/${path}`, `public/nm/${path}`);
+let copyNM = (path) => fs.copySync(`node_modules/${path}`, `public/nm/${path}`);
+let copyBC = (path) => fs.copySync(`bower_components/${path}`, `public/bower_components/${path}`);
+
+// TODO: Be more selective...
+copyBC('');
 
 // <script> tags
-copy('core-js/client/shim.min.js');
-copy('zone.js/dist/zone.js');
-copy('reflect-metadata/Reflect.js');
-copy('systemjs/dist/system.src.js');
+// copy('core-js/client/shim.min.js');
+// copy('zone.js/dist/zone.js');
+// copy('reflect-metadata/Reflect.js');
+// copy('systemjs/dist/system.src.js');
 
 // System.js require()'d
-copy('@angular');
-copy('angular2-in-memory-web-api');
-copy('rxjs');
+// copy('@angular');
+// copy('angular2-in-memory-web-api');
+// copy('rxjs');
